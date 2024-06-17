e3b01f3296fbbb591803e26343594ee50bb250c2
oney2033 <131510065+oney2033@users.noreply.github.com>
2024-06-17 18:58:13 +0800

    近似的求一个函数的积分，并求出50%的积分在哪个点上

--- Ray-Tracing-In-One-Weekend/main.cpp
+++ Ray-Tracing-In-One-Weekend/main.cpp
@@ -19,16 +19,47 @@
 
 using namespace std;
 
-int main() 
+struct sample
 {
-	int a = 0, b = 2;
-	int N = 1000000;
-	auto sum = 0.0;
+	double x;
+	double p_x;
+};
+
+bool compare_by_x(const sample& a, const sample& b)
+{
+	return a.x < b.x;
+}
+
+int main()
+{
+	int N = 10000;
+	double sum = 0;
+	std::vector<sample>samples;
 	for (int i = 0; i < N; i++)
 	{
-		auto x = random_double(a, b);
-		sum += x * x;
+		auto x = random_double(0, 2 * pi);
+		auto sin_x = sin(x);
+		auto p_x = exp(-x / (2 * pi)) * sin_x * sin_x;
+		sum += p_x;
+		sample this_sample = { x,p_x };
+		samples.push_back(this_sample);
+	}
+
+	std::sort(samples.begin(), samples.end(), compare_by_x);
+	double half_sum = sum / 2.0;
+	double halfway_point = 0.0;
+	double accum = 0.0;
+	for (int i = 0; i < N; i++)
+	{
+		accum += samples[i].p_x;
+		if (accum >= half_sum)
+		{
+			halfway_point = samples[i].x;
+			break;
+		}
 	}
 	std::cout << std::fixed << std::setprecision(12);
-	std::cout << "I = " << (b - a) * (sum / N) << "\n";
+	std::cout << "Average = " << sum / N << '\n';
+	std::cout << "Area under curve = " << 2 * pi * sum / N << '\n';
+	std::cout << "Halfway = " << halfway_point << '\n';
 }
